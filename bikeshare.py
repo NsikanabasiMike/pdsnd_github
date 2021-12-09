49fffbcf3dbb002f468bbe26a057fc77fc5c0e75
Nsikanabasi <essietnsikan4@gmail.com>
2021-12-09 15:55:02 +0100

    Add clarity to restart object in main function

--- bikeshare.py
+++ bikeshare.py
@@ -166,7 +166,7 @@ def main():
         user_stats(df)
         raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Any input other than "Yes" will terminate:\n')
         if restart.lower() != 'yes':
             break
 
